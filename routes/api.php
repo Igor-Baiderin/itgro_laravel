c91773929693f1bf2515705cf914cb2a9123aeb3
Igor Baiderin <ibaid@mail.ru>
2024-10-07 09:07:10 +0500

    change route

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/autors', [AutorController::class, 'index']); // Получение всех авторов
-Route::get('/autors/{id}', [AutorController::class, 'show']); // Получение автора по ID
-Route::post('/autors', [AutorController::class, 'store']); // Создание автора
-Route::put('/autors/{id}', [AutorController::class, 'update']); // Обновление автора
-
-Route::get('/books/{id}', [BookController::class, 'show']); // Получение книги по ID
-Route::get('/books', [BookController::class, 'index']); // Получение списка книг
-Route::post('/books', [BookController::class, 'store']); // Создание книги
-Route::put('/books/{id}', [BookController::class, 'update']); // Обновление книги
+Route::resource('/autors', AutorController::class)->only(['index', 'show', 'store', 'update']);
+Route::resource('/books', BookController::class)->only(['index', 'show', 'store', 'update']);
